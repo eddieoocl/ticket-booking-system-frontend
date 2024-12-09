1eb0681878fc72e9a10ca4d3ce1ce71242c01ca3
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-09 16:09:38 +0800

    [Confirm Order Selection] feat: add ConfirmLoading component and css

---
+++ src/app/components/ConfirmLoading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/ConfirmLoading.css";
+
+const ConfirmLoading: React.FC = () => {
+    return (
+        <div className="loading-container d-flex justify-content-center align-items-center">
+            <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    );
+};
+
+export default ConfirmLoading;
