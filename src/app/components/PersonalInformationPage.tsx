3bed97659f9644512b1cfa59723f073ef27a95dd
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-09 16:10:30 +0800

    [Confirm Order Selection] feat: add PersonalInfomationPage component and css

---
+++ src/app/components/PersonalInformationPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/PersonalInfomationPage.css";
+
+const ConfirmLoading: React.FC = (props) => {
+    const {  personal_information} = props;
+    const {first_name, last_name, email, phone} = personal_information;
+    return (
+        <div className="personal-info-container">
+            <h1>Personal Information</h1>
+            <p>Name: {first_name}{last_name}</p>
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+        </div>
+    );
+};
+
+export default ConfirmLoading;
