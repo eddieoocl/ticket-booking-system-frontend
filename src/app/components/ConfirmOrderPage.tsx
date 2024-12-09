c15fff84e47c89913abb3ab344b48fd409289d2f
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-09 17:34:31 +0800

    [Confirm Order Selection] feat: display the button to return to previous page and jump to next page

--- src/app/components/ConfirmOrderPage.tsx
+++ src/app/components/ConfirmOrderPage.tsx
@@ -1,26 +1,31 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../style/ConfirmOrderPage.css";
 import data from "../mockdata/data.json";
 import Commodity from "@/app/components/Commodity";
 import PersonalInformationPage from "@/app/components/PersonalInformationPage";
+import TotalCount from "@/app/components/TotalCount";
 
 const ConfirmOrderPage: React.FC = () => {
-    const [Information, setInformation] = useState();
     const { personal_information, tickets, add_ons } = data;
     return (
         <div className="confirm-order-page">
             <PersonalInformationPage personal_information={personal_information} />
             <div className="commodity-container">
-            {tickets.map((ticket) => (
-                <Commodity key={ticket.id} commodity={ticket} />
-            ))}
+                {tickets.map((ticket) => (
+                    <Commodity key={ticket.id} commodity={ticket} />
+                ))}
             </div>
             <div className="commodity-container">
-            {add_ons.map((add_on) => (
-                <Commodity key={add_on.id} commodity={add_on} />
-            ))}
+                {add_ons.map((add_on) => (
+                    <Commodity key={add_on.id} commodity={add_on} />
+                ))}
+            </div>
+            <TotalCount tickets={tickets} add_ons={add_ons} />
+            <div className="button-container">
+                <button onClick={() => window.location.href = "/payment"}>Commit</button>
+                <button onClick={() => window.location.href = "/ticket"}>Cancel</button>
             </div>
         </div>
     );
