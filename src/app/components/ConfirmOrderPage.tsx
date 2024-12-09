b8d014fa26f11d67f989733f3425797250c5b10f
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-09 22:32:14 +0800

    [Confirm Order Selection] feat:add ticket TotalCount and add-ons TotalCount,add more-button

--- src/app/components/ConfirmOrderPage.tsx
+++ src/app/components/ConfirmOrderPage.tsx
@@ -3,29 +3,35 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../style/ConfirmOrderPage.css";
 import data from "../mockdata/data.json";
-import Commodity from "@/app/components/Commodity";
 import PersonalInformationPage from "@/app/components/PersonalInformationPage";
 import TotalCount from "@/app/components/TotalCount";
 import Ticket from "@/app/components/Ticket";
 import AddOn from "@/app/components/AddOn";
 
+//todo set timer
 const ConfirmOrderPage: React.FC = () => {
     const [tickets, setTickets] = useState(data.tickets);
     const [add_ons, setAddOns] = useState(data.add_ons);
+    const [personal_information, setPersonalInformation] = useState(data.personal_information);
 
     return (
         <div className="confirm-order-page">
-            <PersonalInformationPage personal_information={data.personal_information} />
             <div className="commodity-container">
                 {tickets.map((ticket) => (
                     <Ticket key={ticket.id} commodity={ticket} />
                 ))}
+                <p>Ticket Total</p>
+                <TotalCount tickets={tickets}></TotalCount>
             </div>
             <div className="commodity-container">
+                <div><button className="more-button" onClick={() => window.location.href = "/add-ons"}>More Add-Ons</button></div>
                 {add_ons.map((add_on) => (
                     <AddOn key={add_on.id} commodity={add_on} setAddOns={setAddOns} />
                 ))}
+                <p>Add-on Total</p>
+                <TotalCount add_ons={add_ons}></TotalCount>
             </div>
+            <PersonalInformationPage personal_information={personal_information} />
             <TotalCount tickets={tickets} add_ons={add_ons} />
             <div className="button-container">
                 <button onClick={() => window.location.href = "/payment"}>Commit</button>
