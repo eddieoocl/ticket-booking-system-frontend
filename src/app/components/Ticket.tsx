2c54a3c026d619e295737089ad31b0fa75555247
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-10 00:24:21 +0800

    [Confirm Order Selection] refactor:add type limitation of ticket type prop

--- src/app/components/Ticket.tsx
+++ src/app/components/Ticket.tsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../style/Commodity.css";
+import "@/app/styles/Commodity.css";
+import { Ticket } from "../../types/model/Ticket";
 import React from "react";
 
-const Ticket: React.FC = (props) => {
+const Ticket: React.FC<{ commodity: Ticket }> = (props) => {
     const { commodity } = props;
-    const { name, price, amount, description, seating } = commodity;
-    const [commodityAmount, setCommodityAmount] = React.useState(amount);
+    const { name, price, amount, description, seating } = props.commodity;
+    const [commodityAmount, setCommodityAmount] = React.useState<number>(amount);
     const total = (price * commodityAmount).toFixed(2);
     return (
         <div className="order-entry">
---
+++ src/types/model/Ticket.ts
@@ -0,0 +1,7 @@
+export interface Ticket {
+    name: string;
+    price: number;
+    amount: number;
+    description: string;
+    seating?: string;
+}
