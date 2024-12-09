e0fe8a6626d61df239c78476ee8988717e0c6cfc
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-09 17:35:22 +0800

    [Confirm Order Selection] fix:adjust style

--- src/app/components/PersonalInformationPage.tsx
+++ src/app/components/PersonalInformationPage.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../style/PersonalInfomationPage.css";
 
-const ConfirmLoading: React.FC = (props) => {
-    const {  personal_information} = props;
-    const {first_name, last_name, email, phone} = personal_information;
+const PersonalInformationPage: React.FC = (props) => {
+    const { personal_information } = props;
+    const { first_name, last_name, email, phone } = personal_information;
     return (
         <div className="personal-info-container">
-            <h1>Personal Information</h1>
-            <p>Name: {first_name}{last_name}</p>
+            <h1>Your Personal Information</h1>
+            <p>Name: {first_name} {last_name}</p>
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
         </div>
     );
 };
 
-export default ConfirmLoading;
+export default PersonalInformationPage;
