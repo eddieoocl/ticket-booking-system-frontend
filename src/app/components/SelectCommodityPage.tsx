073de38efa440d0668beec6e6c44cc42afa89969
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-12-10 15:08:32 +0800

    [commodity selection] feat: change commodity selection page

--- src/app/components/SelectCommodityPage.tsx
+++ src/app/components/SelectCommodityPage.tsx
@@ -12,14 +12,19 @@ import AddOn from "@/app/components/AddOn";
 const SelectCommodityPage: React.FC = () => {
     const [tickets, setTickets] = useState(data.ticketsType);
     const [add_ons, setAddOns] = useState(data.add_ons);
+<<<<<<< HEAD
     const [personal_information, setPersonalInformation] = useState(
         data.personal_information
     );
+=======
+    const [personal_information, setPersonalInformation] = useState(data.personal_information);
+>>>>>>> 3d9e090 ([commodity selection] feat: change commodity selection page)
 
     return (
         <div className="confirm-order-page">
             <div className="commodity-container">
                 {tickets.map((ticket) => (
+<<<<<<< HEAD
                     <Ticket
                         key={ticket.id}
                         commodity={ticket}
@@ -51,6 +56,20 @@ const SelectCommodityPage: React.FC = () => {
                         commodity={add_on}
                         setAddOns={setAddOns}
                     />
+=======
+                    <Ticket key={ticket.id} commodity={ticket} setTickets={setTickets} setPersonalInformation={setPersonalInformation}/>
+                ))}
+                <TotalCount tickets={tickets}></TotalCount>
+            </div>
+            <PersonalInformationPage tickets={tickets} personal_information={personal_information} setPersonalInformation={setPersonalInformation} />
+            <div className="commodity-container">
+                <div>
+                    <button className="side-button" onClick={() => window.location.href = "/ConfirmLoading"}>More Add-Ons
+                    </button>
+                </div>
+                {add_ons.map((add_on) => (
+                    <AddOn key={add_on.id} commodity={add_on} setAddOns={setAddOns} />
+>>>>>>> 3d9e090 ([commodity selection] feat: change commodity selection page)
                 ))}
 
                 <TotalCount add_ons={add_ons}></TotalCount>
@@ -59,6 +78,7 @@ const SelectCommodityPage: React.FC = () => {
 
             {/*todo commit call api*/}
             <div className="button-container">
+<<<<<<< HEAD
                 <button onClick={() => (window.location.href = "/concert")}>
                     Return
                 </button>
@@ -67,9 +87,17 @@ const SelectCommodityPage: React.FC = () => {
                 >
                     Next
                 </button>
+=======
+                <button onClick={() => window.location.href = "/concert"}>Return</button>
+                <button onClick={() => window.location.href = "/confirm"}>Next</button>
+>>>>>>> 3d9e090 ([commodity selection] feat: change commodity selection page)
             </div>
         </div>
     );
 };
 
+<<<<<<< HEAD
 export default SelectCommodityPage;
+=======
+export default SelectCommodityPage;
+>>>>>>> 3d9e090 ([commodity selection] feat: change commodity selection page)
